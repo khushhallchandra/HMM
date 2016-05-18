36e005f31431d92c8ba5c423e83ac78abdfff6a9
Khushall Chandra Mahajan <kcm00000@gmail.com>
2016-05-18 16:59:02 +0000

    Done

--- generateSeq.py
+++ generateSeq.py
@@ -1,25 +1,30 @@
 import numpy as np
 
-# P is a list containg probabilty of the states 
-# P = [p1,p2]
-# p1 probability for +1
-# We have only 2 posible states
+# pi It is the probability of occurance of +1
+# P is a 2d list containg probabilty of the states transition
+# P = [[++,+-],[-+,--]]
+# We have only 2 posible states, hence 4 possible transitions
 # T is the length of state sequence
-# The states are represented by numbers -1 and 1
-# The function returns a numpy array of length T
-def genSeq(P,T):
-    #P = np.array(P)
-    #P = np.sort(P)
-    r = np.random.uniform(0,1,T)
-    vecS = np.vectorize(randGen) 
-    return vecS(P[0], r)
+# The states are represented by numbers -1 and +1
+# The function returns a list of length T
+
+def genSeq(pi,P,T):
+    preState = 1
+    r = np.random.uniform(0,1)
+    if(r>=pi):
+        preState = -1
+    r = np.random.uniform(0,1,T-1)
+    seq = [preState]
+    for i in range(T-1):
+        if(preState==1):
+            seq.append(genState(P[0][0],r[i]))
+        else:
+            seq.append(genState(P[1][0],r[i]))
+        preState = seq[i+1]
+    return seq
     
-def randGen(P,r):
-    #r = np.random.uniform(0,1)
-    if(P<r):
+def genState(P,r):
+    if(r<P):
         return 1
     else:
         return -1
-print "Done"
-a = [0.4,0.6]
-S = genSeq(a,500)
---
+++ stateProb.py
@@ -0,0 +1,3 @@
+import numpy as np
+# This function returns the 
+def stateProb(S,auc,c)
